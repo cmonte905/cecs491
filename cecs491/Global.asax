fbee2a0af817c5094b93260294af9cbca557897c
Cesar Montelongo <cmonte905@hotmail.com>
2017-02-01 15:57:55 -0800

    First commit, empty MVC project

---
+++ cecs491/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cecs491.MvcApplication" Language="C#" %>
---
+++ cecs491/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cecs491.Startup))]
+namespace cecs491
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
